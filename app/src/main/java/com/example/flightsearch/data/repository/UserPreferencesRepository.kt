af4de09599e5c0922e74064328c0a74788f5d422
racc00n-ann <72782461+racc00n-ann@users.noreply.github.com>
2025-05-26 18:25:49 +0400

    commit

--- app/src/main/java/com/example/flightsearch/data/repository/UserPreferencesRepository.kt
+++ app/src/main/java/com/example/flightsearch/data/repository/UserPreferencesRepository.kt
@@ -6,7 +6,6 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import javax.inject.Inject
 
 interface UserPreferencesRepository {
     val searchQuery: Flow<String>
